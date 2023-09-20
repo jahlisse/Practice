7558aa0db8a2b0a6d95dbd66bbfa708f9c3eb947
Jahlisse <107271703+jahlisse@users.noreply.github.com>
2023-09-20 14:40:01 -0400

    beginning practice

---
+++ Rpractice.r
@@ -0,0 +1,55 @@
+"Hello world"
+name<- "Steve Smith"
+name
+print(name)
+text<- "Goes to the beach on friday"
+paste(text, name)
+paste(name, text)
+selenagomezsong<- "tell me tell me tell me"
+lyric<- "something I dont know"
+paste(selenagomezsong, lyric)
+#Okay 
+num1<- 32+44
+num2<-32-44
+num1+num2
+var1<-var2<-var3<-"potato"
+var3
+var1
+var2
+x<- 2
+y<- 24
+x
+y
+worldtour<- "RENAISSANCE" 
+worldtour
+min(22,23,24,25)
+max(100000000,2,1,10)
+sqrt(20002)
+abs(-400)
+ceiling(100.2)
+floor(100.2)
+"hello"
+#vs
+'hello'
+#thesame
+str<- "hello"
+str
+str<- "beyonce beyonce beyonce,
+beyonce number 2,
+wooooo beyonce"
+str
+#oh there is the last one
+cat(str)
+nchar(str)
+grepl("p", str)
+str2<-"croissant"
+str2
+\\
+a<- 10
+b<- 2
+a<b
+if (b>a) {
+  print ("b is less than a")
+} else {
+  print("b is not less than a")
+}
